c72df998ae86e05872bcf762a58f2be602ee0392
sargonkadnezar <jungewas40@gmail.com>
2024-10-15 11:59:55 +0200

    Update fuhrwerk.py

--- fuhrwerk.py
+++ fuhrwerk.py
@@ -1,6 +1,6 @@
 fuhrwerk = {
-    "brand": "demo",
-    "model": "irrelevant",
+    "brand": "Benz",
+    "model": "AMG187",
 	"year": "2004"
 	"country": "Österreich"
 	"performance": "500"
@@ -9,6 +9,9 @@ fuhrwerk = {
 }
 
 # opening a file this way is not best practice. but more intuitive for beginners ;-)
-file = open("furhwerk.csv", "w")
+file = open("fuhrwerk.csv", "w")
+file.write("{")
+
 file.write(fuhrwerk["brand"] + ";" + fuhrwerk["model"] + ";" + fuhrwerk["releasedate"]+ ";" + fuhrwerk["performance"]+ ";" + fuhrwerk["country"]+ ";" + fuhrwerk["year"])
+file.write("}")
 file.close()
